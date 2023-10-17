275a8bd87e2d078c091a70223fc2db2883102eb2
SiriusBack <siriusdev@outlook.com>
2023-10-16 23:52:59 -0300

    fix executable path

--- archzen.sh
+++ archzen.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 sudo curl -sSO https://github.com/SiriusBack/archzen/releases/download/v1.0.0/ArchZen.AppImage &&
-    sudo chmod +x /bin/ArchZen.AppImage &&
-    echo "#!/bin/bash" | sudo tee -a /bin/archzen &&
-    echo "sudo /bin/ArchZen.AppImage" | sudo tee -a /bin/archzen &&
-    sudo chmod +x /bin/archzen &&
+    sudo chmod +x ./ArchZen.AppImage &&
+    echo "#!/bin/bash" | sudo tee -a ./archzen &&
+    echo "sudo ./ArchZen.AppImage" | sudo tee -a ./archzen &&
+    sudo chmod +x ./archzen &&
     echo &&
     echo "ArchZen installed successfully!" &&
     echo "You can run using the command: ./archzen" &&
