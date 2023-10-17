ac7bb19c5b33e74487d441d33d41e01d9c192820
SiriusBack <siriusdev@outlook.com>
2023-10-17 00:46:46 -0300

    move main code to ArchZen repo

--- archzen.sh
+++ archzen.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
 
-# curl -sSL https://siriusback.github.io/archzen.sh | bash
-# or
-# wget -qO- https://siriusback.github.io/archzen.sh | bash
-
-if [ "$(id -u)" != 0 ]; then
-    echo "Please run this script as root"
-    exit 1
-
-fi
-
-set -e
-
-echo
-echo "Installing ArchZen..."
-echo
-
-pacman -S --noconfirm --needed wget
-
-wget https://github.com/SiriusBack/archzen/releases/download/v1.0.0/ArchZen.AppImage
-
-mv ./ArchZen.AppImage /bin
-
-echo "#!/bin/bash" | sudo tee -a ./bin/archzen
-echo "/bin/ArchZen.AppImage" | sudo tee -a ./bin/archzen
-chmod +x /bin/archzen
-
-echo
-echo "ArchZen installed successfully!"
-echo "You can run using the command: archzen"
-echo
+curl -sSL https://raw.githubusercontent.com/SiriusBack/archzen/main/scripts/install_appimage.sh | sudo bash
